2277553b845c7a2cfc4b66d0660bfa662968061f
jin <infinitus.internal@gmail.com>
2011-07-12 10:58:00 +0800

    removed unused functions and views

--- carparks/views.py
+++ carparks/views.py
@@ -12,17 +12,6 @@ def measurements(request, template_name='measurements.html'):
 
 
 @csrf_exempt
-def record(request, req_cluster, req_node, req_reading):
-    try:
-        req_cluster, req_node, req_reading = int(req_cluster), int(req_node), int(req_reading)
-    except ValueError:
-        raise Http404()
-    record = Measurement(node = Node.objects.get(cluster = req_cluster, node = req_node), raw_reading = req_reading)
-    record.save()
-    return HttpResponse('OK')
-
-
-@csrf_exempt
 def node_request(request, req_cluster, req_node):
     try:
         req_cluster, req_node, = int(req_cluster), int(req_node)
@@ -34,8 +23,8 @@ def node_request(request, req_cluster, req_node):
         for element in tree.getiterator():
             if element.tag == 'reading':
                 req_reading = int(element.text)
-        record = Measurement(node = Node.objects.get(cluster = req_cluster, node = req_node), raw_reading = req_reading)
-        record.save()
+        entry = Measurement(node = Node.objects.get(cluster = req_cluster, node = req_node), raw_reading = req_reading)
+        entry.save()
         return HttpResponse('OK\n')
 
     elif request.method == 'GET':
--- urls.py
+++ urls.py
@@ -2,20 +2,14 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 admin.autodiscover()
 
+# Django-related/Experimental
 urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
 )
 
 
-'''Experimental'''
-urlpatterns += patterns('ep.views',
-        (r'^helloworld/$', 'helloworld'),
-        )
-
-
-''' Easypark '''
+# EP-related
 urlpatterns += patterns('ep.carparks.views',
         (r'^live/$', 'measurements'),
-        (r'^(?P<req_cluster>\d{1,2})/(?P<req_node>\d{1,2})/(?P<req_reading>\d{1,3})/$', 'record'),
         (r'^(?P<req_cluster>\d{1,2})/(?P<req_node>\d{1,2})/$', 'node_request'),
         )
