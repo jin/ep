541615d25f0a5f79a4067ad93c83bd63fd7c1521
jin <infinitus.internal@gmail.com>
2011-07-15 14:00:32 +0800

    removed extra slash

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls.defaults import *
 
 
 urlpatterns = patterns('ep.api.handlers',
-        (r'/xml/clusters', 'xml_response'),
+        (r'xml/clusters', 'xml_response'),
         )
