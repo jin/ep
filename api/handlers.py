ff0ad98f476e0ad7702fbbc5b04655c408da006a
jin <infinitus.internal@gmail.com>
2011-07-19 14:56:42 +0800

    simple working example of using django-piston to return a JSON response for a HTTP GET request to /api/piston/clusters/

--- api/handlers.py
+++ api/handlers.py
@@ -1,49 +1,15 @@
-from piston.handler import BaseHandler, Emitter
+from piston.handler import BaseHandler
 from ep.carparks.models import Node, Measurement
 
 
 class MeasurementHandler(BaseHandler):
-    allowed_methods = ('GET',)
+    allowed_methods = ('GET','POST')
+    model = Measurement
 
-    @classmethod
     def read(self, request):
+        entries = Measurement.objects
 
-        req_cluster = int(request.GET["cluster_id"])
-
-        try:
-            latest = request.GET["latest"]
-            if int(latest) >= 250:
-                latest = 250
-        except:
-            latest = None
-
-        try:
-            req_node = int(request.GET["node_id"])
-        except:
-            req_node = None
-
-        if req_node: 
-            selected_node = Node.objects.filter(cluster = req_cluster).get(node = req_node)
-            if latest:
-                measurements = Measurement.objects.filter(node = selected_node.id)[:latest]
-            else:
-                measurements = Measurement.objects.filter(node = selected_node.id)
-
-        else:
-            selected_nodes = Node.objects.filter(cluster = req_cluster)
-            nodes = []
-            for node in selected_nodes:
-                nodes.append(node.id)
-            if latest:
-                measurements = Measurement.objects.filter(node__in=nodes)[:latest]
-            else:
-                measurements = Measurement.objects.filter(node__in=nodes)
-
-        return measurements
+        return entries.all()[:25]
+    
 
 
-class XMLEmitter(Emitter):
-    pass
-
-
-Emitter.register('xml', XMLEmitter, 'text/xml; charset=utf-8')
--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls.defaults import *
+from piston.resource import Resource
+from ep.api.handlers import MeasurementHandler
 
 
 # Using views.py for API
@@ -6,7 +8,8 @@ urlpatterns = patterns('ep.api.views',
         (r'xml/clusters', 'process_xml'),
         )
 
+measurement_handler = Resource(MeasurementHandler)
 # Using handlers.py for API for django-piston
-#urlpatterns += patterns('ep.api.handlers',
-        #(r'xml/clusters', 'xml_response'),
-        #)
+urlpatterns += patterns('',
+        (r'piston/clusters', measurement_handler),
+        )
