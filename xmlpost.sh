0c2bb415ff892f7d5ba77c4c8fbae1ce1e6b3e46
jin <infinitus.internal@gmail.com>
2011-07-19 12:50:09 +0800

    updated xmlpost.sh to post to the new api url

--- xmlpost.sh
+++ xmlpost.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-curl -H "Content-Type: text/xml" -d "<?xml version='1.0' encoding='utf-8'?><node><reading>300</reading></node>" http://localhost:8000/live/1/5/
+curl -H "Content-Type: text/xml" -d "<?xml version='1.0' encoding='utf-8'?><data><reading>300</reading><batt>50</batt><seq>50</seq><cluster>2</cluster><node>4</node></data>" http://192.168.1.4:8000/api/xml/clusters
+
+
