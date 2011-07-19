239c5a16b122b8186cd381711814c27509553da7
jin <infinitus.internal@gmail.com>
2011-07-19 14:37:47 +0800

    added: get XML data without parameters

--- api/views.py
+++ api/views.py
@@ -9,28 +9,32 @@ from django.http import Http404, HttpResponse, HttpResponseRedirect
 @csrf_exempt
 def process_xml(request):
     if request.method == 'GET':
-        req_cluster = int(request.GET["cluster_id"])
-
+        try:
+            req_cluster = int(request.GET["cluster_id"])
+        except:
+            req_cluster = None
         try:
             latest = request.GET["latest"]
             if int(latest) >= 250:
                 latest = 250
         except:
             latest = None
-
         try:
             req_node = int(request.GET["node_id"])
         except:
             req_node = None
 
-        if req_node: 
+        if req_node and req_cluster: 
             selected_node = Node.objects.filter(cluster = req_cluster).get(node = req_node)
             if latest:
                 measurements = Measurement.objects.filter(node = selected_node.id)[:latest]
             else:
                 measurements = Measurement.objects.filter(node = selected_node.id)
 
-        else:
+        elif req_node and latest and req_cluster == None:
+            return HttpResponse("Please specify the cluster_id.")
+
+        elif req_cluster:
             selected_nodes = Node.objects.filter(cluster = req_cluster)
             nodes = []
             for node in selected_nodes:
@@ -39,6 +43,12 @@ def process_xml(request):
                 measurements = Measurement.objects.filter(node__in=nodes)[:latest]
             else:
                 measurements = Measurement.objects.filter(node__in=nodes)
+
+        elif latest:
+            measurements = Measurement.objects.all()[:latest]
+
+        else:
+            measurements = Measurement.objects.all()[:50]
 
         return render_to_response('measurements.xml', {'measurements': measurements})
 
